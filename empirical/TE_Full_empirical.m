b82c922628cb59afae724f59e0fb9788f9acf966
Varun Madan Mohan <137464587+vmadanmohan@users.noreply.github.com>
2024-08-07 13:22:45 +1000

    Update TE_Full_empirical.m
    load pre-remapped data from cleanup.py - does away having to remap the final matrices before loading them into TE_mean and TE_mean_subjectlevelstd

--- empirical/TE_Full_empirical.m
+++ empirical/TE_Full_empirical.m
@@ -11,8 +11,7 @@ numepochs=zeros(subjects,1);
 parfor sub=1:subjects
     recording=load(sprintf("/path/to/%d_resting.mat",sub));
     main_data=recording.main_data;
-    mapping=load('mapping.txt');
-    delay=load(sprintf("/path/to/%d_delayfile.txt",sub));
+    delay=recording.delay;
     epoch = 10*Fs;              % 10 second epochs
     win = 1*Fs;                  % 1 second window
     numepochs(sub)=floor(size(main_data,1)/epoch);
@@ -21,6 +20,6 @@ parfor sub=1:subjects
         data = main_data(((s-1)*epoch)+1:(s*epoch),:);
         TE(:,:,s)=TransferEntFull(data,N,delay,teCalc);
     end
-    TE_mean(:,:,sub)=matrestruct(mean(TE,3),mapping);
-    TE_mean_subjectlevelstd(:,:,sub)=matrestruct(std(TE,0,3),mapping);
+    TE_mean(:,:,sub)=mean(TE,3);
+    TE_mean_subjectlevelstd(:,:,sub)=std(TE,0,3);
 end
