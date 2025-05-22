d96c9eacd0e20f66168d2a823205a51c783c965c
Varun Madan Mohan <137464587+vmadanmohan@users.noreply.github.com>
2025-05-22 17:57:25 +1000

    Create PearsonEWCNetwork.m

---
+++ flex_comm_principles/observed/PearsonEWCNetwork.m
@@ -0,0 +1,26 @@
+% Function to infer communication between pairs of anatomically connected regions using the partial correlation, implemented as per the Event-marked Windowed Communication protocol
+function [P,P_std] = PearsonEWCNetwork(data,SC,win,N,delay,sigevents)
+    P = zeros(N,N);
+    P_std = zeros(N,N);
+    for i=1:N          % Loop over source regions
+        reldelays = delay(i,:);  % Delays relative to the source region
+        signum = sum(sigevents(:,i),1);  % Number of significant events
+        if signum~=0
+            sigind = find(sigevents(:,i));  % Indices of significant events
+            totevents=signum;
+            P_event = zeros(totevents,N);
+            while signum            % While loop starting from the last significant event and moving backwards
+                for j=find(SC(i,:))   % Loop over regions that are anatomically connected to the source
+                    [I,pval] = partialcorr(data(sigind(signum):sigind(signum)+win-1,i),data(sigind(signum)+reldelays(j):sigind(signum)+reldelays(j)+win-1,j),data(sigind(signum)+reldelays(j)-win:sigind(signum)+reldelays(j)-1,j));  % Partial correlation calculation
+                    if pval>=0.01/totevents  % Applying a Bonferroni correction to account for multiple events
+                        P_event(signum,j)=0;
+                    else
+                        P_event(signum,j)=I;
+                    end
+                end
+                signum=signum-1;
+            end
+            P(i,:)=mean(P_event,1);  % Mean PC over all significant events in source region
+            P_std(i,:)=std(P_event,0,1);
+        end
+    end
