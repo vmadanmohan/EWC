933cdf14b7b27e1ae3ea6c03179d93847b48c04c
Varun Madan Mohan <137464587+vmadanmohan@users.noreply.github.com>
2025-10-06 17:49:02 +1100

    Update surr_correction.m

--- flex_comm_principles/surr_correction.m
+++ flex_comm_principles/surr_correction.m
@@ -2,16 +2,16 @@
 sub=1; % subject index
 surr_comp=[];
 comm_principle_surrcorr=[];
-result=load(sprintf("/path/to/observed_results/%d_results.mat",sub));
+result=load(sprintf("observed/%d_results.mat",sub));
 obs_comm=result.comm_principle_mean;
-for surr=1:1000
-    surrresult=load(sprintf("/path/to/surrogate/results/%d/%d_results_surr.mat",sub,surr));
+for surr=1:Nsurr
+    surrresult=load(sprintf("surrogate/surrogate_results/%d/%d_results_surr.mat",sub,surr));
     surr_comp(:,:,surr)=abs(surrresult.comm_principle_mean_surr)>=abs(obs_comm); % check instances where surrogate relationship is stronger than the observed relationship
 end
-surr_comp=sum(surr_comp,3)/1000;
+surr_comp=sum(surr_comp,3)/Nsurr;
 for i=1:10 % 10 = total number of oscillatory measures we compute
     surr_comp(:,i)=mafdr(surr_comp(:,i),'BHFDR',true);
 end
 mask=surr_comp<0.05;
 comm_principle_mean_surrcorr=obs_comm.*mask;
-save(sprintf("/path/to/output/dir/%d_results_surrcorr",sub),"comm_principle_mean_surrcorr")
+save(sprintf("observed/%d_results_surrcorr",sub),"comm_principle_mean_surrcorr")
